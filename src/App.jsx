312e087401d271c41f3672001eb3e8780e77f85d
yudiz_DARSHAN <darshan.bhensadadiya@yudiz.com>
2022-11-18 19:15:29 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Product from './Pages/Products'
+
+function App () {
+  return (
+    <>
+<Product/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Products/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Products.scss'
+
+function Product () {
+  return (
+        <div className='main_container'>
+            <div className="app">
+                <span>Darshan</span>
+            </div>
+        </div>
+  )
+}
+export default Product
